f0c3802227694d3f65baa737b6e2ba76b300b4fe
m1issa <m1issa@ucsd.edu>
2017-07-18 12:29:23 -0700

    Update purge.c

--- surrogate/purge.c
+++ surrogate/purge.c
@@ -8,7 +8,6 @@
 #include "blake2/sse/blake2-impl.h"
 
 const unsigned int FLAGS = MDB_DUPSORT | MDB_DUPFIXED;
-const int NUM_BYTES = 17;
 const int HASH_BYTES = 8;
 int byte_to_hex (char outstr[], char * instr);
 
@@ -18,10 +17,9 @@ main(int argc, char * argv[]) {
         // set up variables
         int rc, i = 0, j, image_number = 0, key_number = 0;
         size_t num_images = 0, num_url_keys = 0;
-        char blake_str [HASH_BYTES];
-        char hashed_key [NUM_BYTES];
-        char url_array [NUM_BYTES];
-        char * key_to_delete = malloc (NUM_BYTES);
+        char hashed_key [HASH_BYTES];
+        char url_array [HASH_BYTES];
+        char * key_to_delete = malloc (HASH_BYTES);
         char * hash_status = malloc (HASH_BYTES);
 
         // database variables
@@ -33,9 +31,9 @@ main(int argc, char * argv[]) {
         MDB_stat stats;
 
         //set up key to look up
-        key.mv_size = NUM_BYTES;
+        key.mv_size = HASH_BYTES;
         key.mv_data = &hashed_key;
-        url.mv_size = NUM_BYTES;
+        url.mv_size = HASH_BYTES;
         url.mv_data = &url_array;
 
         // set up key
@@ -59,10 +57,7 @@ main(int argc, char * argv[]) {
 
         if ((strcmp (hash_status, "no")) == 0) {
                 // hash input string to key
-                rc = blake2b (blake_str, HASH_BYTES, key_to_delete, strlen(key_to_delete), hash_key, HASH_BYTES);
-                assert (rc == 0);
-
-                rc = byte_to_hex (hashed_key, blake_str);
+                rc = blake2b (hashed_key, HASH_BYTES, key_to_delete, strlen(key_to_delete), hash_key, HASH_BYTES);
                 assert (rc == 0);
         }
         else if ((strcmp(hash_status, "yes")) == 0) {
@@ -75,55 +70,55 @@ main(int argc, char * argv[]) {
 
         // initialize environment; set 2 database limit
         rc = mdb_env_create (&env);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         rc = mdb_env_set_maxdbs (env, 2);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         rc = mdb_env_open (env, "./db_dir", 0, 0664);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
 
         // begin transaction
         rc = mdb_txn_begin (env, NULL, 0, &txn);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
 
         // open databases
         rc = mdb_dbi_open (txn, "data_store", FLAGS, &dbi);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         rc = mdb_dbi_open (txn, "rev_data_store", FLAGS, &dbi_rev);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
 
         // initiate cursors
         rc = mdb_cursor_open (txn, dbi, &cursor);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         rc = mdb_cursor_open (txn, dbi_rev, &cursor_rev);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         rc = mdb_cursor_open (txn, dbi, &cursor2);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
 
                  /***End of Set Up***/
 
  	// locate key, get first url
         rc = mdb_cursor_get (cursor, &key, &url, MDB_SET | MDB_FIRST_DUP);
-        if (rc != 0) {
+        if (rc != MDB_SUCCESS) {
                 fprintf (stderr, "\nERROR: Key not found\n\n");
                 return -1;
         }
 
         // count, print # of images(URLs) under this input key
         rc = mdb_cursor_count (cursor, &num_images);
-        assert (rc == 0);
+        assert (rc == MDB_SUCCESS);
         fprintf (stdout, "\nThis key has %zu image(s)\n", num_images);
 
         while ( image_number < num_images ) {
                 // get 1st key from reverse mapped database
                 rc = mdb_cursor_get (cursor_rev, &url, &new_key, MDB_SET_KEY | MDB_FIRST_DUP);
-                if (rc != 0) {
+                if (rc != MDB_SUCCESS) {
                         fprintf (stderr, "\nERROR: Key's URL not found\n\n");
                         return -1;
                 }
 
                 // get,print count of keys under URL
                 rc = mdb_cursor_count (cursor_rev, &num_url_keys);
-                assert (rc == 0);
+                assert (rc == MDB_SUCCESS);
                 fprintf (stdout, "\nImage %d has %zu keys\n", (image_number+1), num_url_keys);
 
                 // loop through to delete each key
@@ -131,10 +126,10 @@ main(int argc, char * argv[]) {
 
                         //position at specified key and url
                         rc = mdb_cursor_get (cursor2, &new_key, &url, MDB_GET_BOTH);
-                        if (rc == 0) {
+                        if (rc == MDB_SUCCESS) {
                                 //fprintf (stdout, "Deleting from %s: Key %s \tURL %s\n", key.mv_data, new_key.mv_data, url.mv_data);
                                 rc = mdb_cursor_del (cursor2, 0);
-                                assert (rc == 0);
+                                assert (rc == MDB_SUCCESS);
                                 i++; //delete total number of items deleted
                         }
                         else
@@ -147,27 +142,27 @@ main(int argc, char * argv[]) {
                         else {
                                 // delete URL in reverse mapped data map and all its entries
                                 rc = mdb_cursor_del (cursor_rev, MDB_NODUPDATA);
-                                assert (rc == 0);
+                                assert (rc == MDB_SUCCESS);
                                 break;
                         }
                 }
                 //commit transaction
 		rc = mdb_txn_commit (txn);
-                assert (rc == 0);
+                assert (rc == MDB_SUCCESS);
 
                 // reset transaction
                 rc = mdb_txn_begin (env, NULL, 0, &txn);
-                assert (rc == 0);
+                assert (rc == MDB_SUCCESS);
 
                 // re-initiate cursor
                 rc = mdb_cursor_open (txn, dbi, &cursor);
                 rc = mdb_cursor_open (txn, dbi_rev, &cursor_rev);
                 rc = mdb_cursor_open (txn, dbi, &cursor2);
-                assert (rc == 0);
+                assert (rc == MDB_SUCCESS);
 
                 // get next image
 
-                if ((mdb_cursor_get (cursor, &key, &url, MDB_SET | MDB_NEXT_DUP)) == 0) {
+                if ((mdb_cursor_get (cursor, &key, &url, MDB_SET | MDB_NEXT_DUP)) == MDB_SUCCESS) {
                         image_number++;
                         key_number = 0;
                 }
@@ -191,13 +186,3 @@ main(int argc, char * argv[]) {
 
         return 0;
 }
-
-int byte_to_hex (char outstr[], char * instr) {
-        int i;
-        for (i = 0; i < sizeof (instr); i++) {
-                sprintf (&outstr[2 * i], "%02X",  instr[i]);
-        }
-        outstr [(NUM_BYTES - 1)] = '\0';
-        return 0;
-}
-
